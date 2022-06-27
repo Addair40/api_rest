4ab1810d6b6673822ebe30efb92776c5643208fa
Addair40 <71908038+Addair40@users.noreply.github.com>
2022-06-27 11:16:18 -0500

    Update clientes.sql

--- código/sql/clientes.sql
+++ código/sql/clientes.sql
@@ -1,1 +1,15 @@
+DROP TABLE IF EXISTS clientes;
 
+CREATE TABLE clientes(
+    id_cliente INTEGER PRIMARY KEY  AUTOINCREMENT,
+    nombre     VARCHAR(50) NOT NULL,
+    email      VARCHAR(50) NOT NULL
+    );
+
+INSERT INTO clientes (nombre,email) VALUES
+("Addair","addair@email.com"),
+("Ignacio","ignacio@email.com"),
+("Miguel","miguel@email.com");
+.headers ON
+
+SELECT * FROM clientes;
